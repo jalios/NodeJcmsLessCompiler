2915de0df573e2d3e6edb70bc84c287a33bedecf
tequila <kevin92320@gmail.com>
2017-01-30 16:34:20 +0100

    Clean up code and add comments

--- getJcmsPropfiles.js
+++ getJcmsPropfiles.js
@@ -1,38 +1,36 @@
-var fs = require('fs'),
-less = require('less'),
-path = require('path'),
-lessPropDeclarationRegex = new RegExp(/^channel\.less\.(.*)[:|=](.*)$/, "gm"),
-mainPluginPropDeclaration = new RegExp(/^channel\.main-plugin:\s*([\w]*)$/, "gm")
-pluginXmlOrderRegex = new RegExp(/order="(\d+)"/, "g")
-rootPath = process.argv[2];
-
+var fs = require('fs'), // File system
+less = require('less'), // Less compiler
+lessPropDeclarationRegex = new RegExp(/^channel\.less\.(.*)[:|=](.*)$/, "gm"), // Regex for less files declaration in properties files
+mainPluginPropDeclaration = new RegExp(/^channel\.main-plugin:\s*([\w]*)$/, "gm") // Regex for main plugin declaration in properties files
+pluginXmlOrderRegex = new RegExp(/order="(\d+)"/, "g") // Regex for plugin order declaration in plugin.xml
+rootPath = process.argv[2]; // Parameter which represents the jcms workspace folder to compile
 
 const WebInfPathSuffix = "/WEB-INF";
 const pluginWebInfPathSuffix = "/plugins";
 const pluginPropPathSuffix = "/properties";
-const notifier = require('node-notifier');
 
-//C:\_DEV\_workspaces\JCMS10.x_ws\JCMS\webapps\en\plugins
-const util = require('util');
-//https://github.com/mikaelbr/node-notifier
-
-//TODO : get mainPlugin from custom.prop : channel.main-plugin
-
+/**
+ * JcmsPlugin object representing a JCMS Plugin
+ */
 function JcmsPlugin(name, order, lessFiles) {
   this.name = name;
   this.order = order;
   this.lessFiles = lessFiles;
 }
 
+/**
+ * PRetty print of the JcmsPlugin object
+ */
 JcmsPlugin.prototype.print = function() {
   console.log("JcmsPlugin.prototype.print");
   console.log(JSON.stringify(this, null, 2))
 };
 
-JcmsPlugin.prototype.getOrder = function() {
-  console.log("I'm the main plugin");
-};
-
+/**
+ * Search the main plugin declaration in given JCMS custom.prop file
+ * @param rootPath the root path where to search for the custom.prop file
+ * @return the mainPlugin name
+ */
 function getMainPlugin(rootPath) {
   console.log("Starting to parse custom.prop for mainplugin : " + rootPath + "/data/custom.prop \n");
   var mainPlugin;
@@ -52,6 +50,11 @@ function getMainPlugin(rootPath) {
   return mainPlugin;
 }
 
+/**
+ * Search for less files to compile in given JCMS jcms.prop file
+ * @param rootPath the root path where to search for the jcms.prop file
+ * @return json array of less files
+ */
 function getJcmsPropLessFiles(rootPath) {
   var jsonObj = {};
 
@@ -70,13 +73,14 @@ function getJcmsPropLessFiles(rootPath) {
   console.log(count + " less files declarations have been found in jcms.prop");
   console.log("\n");
 
-  // if (Object.keys(jsonObj).length > 0) {
-  //   console.log(jsonObj);
-  // }
-
   return jsonObj;
 }
 
+/**
+ * Search for less files to compile in given JCMS custom.prop file
+ * @param rootPath the root path where to search for the custom.prop file
+ * @return json array of less files
+ */
 function getCustomPropLessFiles(rootPath) {
   var jsonObj = {};
   var count = 0;
@@ -101,6 +105,12 @@ function getCustomPropLessFiles(rootPath) {
   return jsonObj;
 }
 
+
+/**
+ * Search for all plugins of the given JCMS folder
+ * @param rootPath the root path of plugins
+ * @return array of JcmsPlugin
+ */
 function getJcmsPlugins(rootPath) {
   var pluginFolder = rootPath + pluginWebInfPathSuffix;
 
@@ -150,24 +160,14 @@ function getJcmsPlugins(rootPath) {
   return plugins;
 }
 
-
-
-var exports = module.exports = {};
-exports.sayHelloInEnglish = function() {
-  return "HELLO";
-};
-
-/*
-jcms.prop
-plugin.prop (Without mainPlugin)
-custom.prop
-mainPlugin.prop
-*/
-
-exports.getAllJcmsPropLessFiles = function () {
-  return getAllJcmsPropLessFiles();
-}
-
+/**
+ * Parse following files to retrieve less files to compile :
+ * - jcms.prop
+ * - plugin.prop (Without mainPlugin)
+ * - custom.prop
+ * - mainPlugin.prop
+ * @return json array of less files
+ */
 function getAllJcmsPropLessFiles() {
   var rootPath = process.argv[2];
   var webInfPath = rootPath + WebInfPathSuffix;
@@ -225,35 +225,16 @@ function getAllJcmsPropLessFiles() {
       }
     }
   }
-  // console.log(lessFiles);
   console.log(Object.keys(lessFiles).length + " less files have been found for all Jcms properties");
-
-
-
-  // SAVE TO FILE
-  // var json = JSON.stringify(lessFiles);
-  //
-  // fs.writeFile("jcms-less-files.json", json ,'utf8', function(err) {
-  //   if(err) {
-  //       return console.log(err);
-  //   }
-  //
-  //   console.log("The file was saved!");
-  // });
-
-  // for (var lessItem in lessFiles) {
-  //   if (lessFiles.hasOwnProperty(lessItem)) {
-  //     compileLESS(rootPath + "/" + lessItem, rootPath + "/" + lessFiles[lessItem]);
-  //   }
-  // }
 
   return lessFiles;
 }
 
-
-
-function watchFolder() {
-  console.log("Starting to watch folder '" + rootPath +"' for updates.");
+/**
+ * Compiles JCMS Less files and write result of the compilation in result.log file
+ * @return status 0 if OK or 1 if failed
+ */
+function compileJCMSLess() {
   var lessFiles = getAllJcmsPropLessFiles();
   var compileLessResult = {};
 
@@ -261,20 +242,12 @@ function watchFolder() {
   var okFileCount = 0;
   var failedFileCount = 0;
   var failedFileList = [];
+
+  console.log("Starting Less compilation...\n");
+
   for (var lessItem in lessFiles) {
     if (lessFiles.hasOwnProperty(lessItem)) {
-      // console.log("YOYO " + path.join(rootPath, lessItem));
-
-      // var filePath = path.join(rootPath, lessItem);
-      // if (fs.existsSync(filePath)) {
-      //   fs.watch(filePath, function (evt, file) {
-      //     console.log(evt, file);
-      //     if (!file || !lessFiles[file]) return;
-      //     if (files[file]) compileLESS(file, lessFiles[file]);
-      //   });
-      // }
       var result = compileLESS(rootPath + "/" + lessItem.trim(), rootPath + "/" + lessFiles[lessItem]);
-      // console.log(JSON.stringify(result, null, 2));
       if (result && result.status === "OK") {
         okFileCount++;
       } else {
@@ -286,36 +259,28 @@ function watchFolder() {
     }
   }
 
-  // Object
-  // notifier.notify({
-  //   'title': 'Less has been compiled',
-  //   'message': 'Hello, there!'
-  // });
-
   compileLessResult['finalStatus'] = {'okCount':okFileCount,'failedFileCount':failedFileCount, 'failedFileList': failedFileList};
+
+  console.log("Less compilation has finished : " + okFileCount + " files have been compiled, " + failedFileCount  + " files have failed.");
+  console.log(failedFileList);
+  console.log("\n");
 
   fs.writeFile("result.log", JSON.stringify(compileLessResult, null, 2), function(err) {
       if(err) {
-          return console.log(err);
+        return console.log(err);
       }
-
-      console.log("The file was saved!");
+      console.log("Result has been saved in result.log file.");
   });
 
   return failedFileCount > 0 ? 1 : 0;
-
-  // return lessFiles;
-  // Watch current folder & compile if file from files{} has changed
-  // fs.watch(rootPath, function (evt, file) {
-  //   file = path.join(rootPath, file);
-  //   console.log(evt, file);
-  //   if (!file || !lessFiles[file]) return;
-  //   if (files[file]) compileLESS(file, lessFiles[file]);
-  // });
 }
 
-watchFolder();
-
+/**
+ * Compiles JCMS Less files
+ * @param from : the from less file to compile
+ * @param to : the to css file to compile
+ * @return a json object representing status of less compilation
+ */
 function compileLESS(from, to) {
   var result = {};
 
@@ -331,12 +296,6 @@ function compileLESS(from, to) {
     return;
   }
 
-  // if (!fs.existsSync(to)) {
-  //   console.log("To file doesn't exists : " + to);
-  //   result = {'from':from, 'to':to,'status':"TO MISSING"};
-  //   return result;
-  // }
-
   if (!fs.existsSync(from)) {
     console.log("From file doesn't exists : " + from);
     result = {'from':from, 'to':to,'status':"FROM MISSING"};
@@ -344,10 +303,7 @@ function compileLESS(from, to) {
     return;
   }
 
-  // less.writeError( "TODO");
-
   var contents = fs.readFileSync(from.trim());
-  // console.log("Compiling file : '" + from.split('\\').pop().split('/').pop() + "' to '" + to.split('\\').pop().split('/').pop() + "'");
 
   less.render(contents.toString(), {
     compress: false,
@@ -363,43 +319,10 @@ function compileLESS(from, to) {
       // result += e;
       result = {'from':from,'to':to,'status':"FAILED", 'errorLog' : e};
     }
-    // console.log(to);
-    //console.log(util.inspect(output, {showHidden: false, depth: null}))
   });
 
   return result;
-  // fs.readFile(from.trim(), function (err, data) {
-  //   console.log("Compiling file : '" + from.split('\\').pop().split('/').pop() + "' to '" + to.split('\\').pop().split('/').pop() + "'");
-  //   if (err){
-  //     console.log("Compile failed" + err);
-  //     return;
-  //   }
-  //   less.render(data.toString(), {
-  //     compress: false,
-  //     filename: from
-  //   }, function (e, output) {
-  //     fs.writeFile("/result.log", "Hey there!", function(err) {
-  //         if(err) {
-  //             return console.log(err);
-  //         }
-  //
-  //         console.log("The file was saved!");
-  //     });
-  //
-  //     if (!e){
-  //       fs.writeFile(to.trim(), output.css, function(){
-  //         console.log("Converted Less: '" + to);
-  //       });
-  //       return result;
-  //     } else{
-  //       console.log(e);
-  //       result += e;
-  //       return result;
-  //     }
-  //     // console.log(to);
-  //     //console.log(util.inspect(output, {showHidden: false, depth: null}))
-  //   });
-  // });
 }
 
-//getAllJcmsPropLessFiles();
+//Execute
+compileJCMSLess();
